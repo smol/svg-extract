4552d3e367d967936a0b2762c9646bccd8c6136c
Julien Smolareck <julien.smolareck@gmail.com>
2016-05-29 23:42:48 +0200

    first commit

---
+++ spec/svg-extract-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SvgExtractView from '../lib/svg-extract-view';
+
+describe('SvgExtractView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
